c761563dc6b5181a15d4ce94292d1b781d373a15
casszzz33 <zzq1095822484@gmail.com>
2024-11-13 14:54:28 -0800

    Add files via upload

---
+++ SortableArray/src/SortableArray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SortableArray {
+    public static int[] sortIntegers(int[] array) {
+        Arrays.sort(array);
+        return array;
+    }
+
+    public static String[] sortStrings(String[] array) {
+        Arrays.sort(array);
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] intArray = {3, 5, 1, 4, 2};
+        String[] stringArray = {"banana", "apple", "cherry", "date"};
+
+        System.out.println("Sorted Integers: " + Arrays.toString(sortIntegers(intArray)));
+        System.out.println("Sorted Strings: " + Arrays.toString(sortStrings(stringArray)));
+    }
+}
